1c83fc310e17ab71874b5ca63784fd13c00d6def
JoeVenner <ylafrimi@gmail.com>
2020-06-15 15:06:38 +0100

    Update Detector.py

--- Detector.py
+++ Detector.py
@@ -6,7 +6,7 @@ def main_app(name):
         face_cascade = cv2.CascadeClassifier('./data/haarcascade_frontalface_default.xml')
         recognizer = cv2.face.LBPHFaceRecognizer_create()
         recognizer.read(f"./data/classifiers/{name}_classifier.xml")
-        cap = cv2.VideoCapture(1)
+        cap = cv2.VideoCapture(0)
 
         while True:
             ret, frame = cap.read()
