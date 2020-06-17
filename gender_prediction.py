72f491cd06c150d054fede469dbfdee0f1c5d718
JoeVenner <ylafrimi@gmail.com>
2020-06-17 13:42:37 +0200

    Add files via upload

---
+++ gender_prediction.py
@@ -0,0 +1,94 @@
+import cv2
+import pafy
+from mtcnn.mtcnn import MTCNN
+from keras.models import load_model
+from keras.preprocessing.image import img_to_array
+import numpy as np
+
+
+
+
+url = 'https://www.youtube.com/watch?v=d2GBO_QjRlo'
+vpafy = pafy.new(url)
+play = vpafy.getbest(preftype="mp4")
+
+frame = cv2.VideoCapture(play.url)
+
+detector= MTCNN()
+
+emotion_model = "./data/_mini_XCEPTION.106-0.65.hdf5"
+ageProto="./data/age_deploy.prototxt"
+ageModel="./data/age_net.caffemodel"
+genderProto="./data/gender_deploy.prototxt"
+genderModel="./data/gender_net.caffemodel"
+
+MODEL_MEAN_VALUES=(78.4263377603, 87.7689143744, 114.895847746)
+ageList=['(0-2)', '(4-6)', '(8-12)', '(15-20)', '(25-32)', '(38-43)', '(48-53)', '(60-100)']
+genderList=['Male','Female']
+Emotions = ["angry","disgust","scared","happy","sad","surprised","neutral"]
+
+face_cascade = cv2.CascadeClassifier('./data/haarcascade_frontalface_default.xml')
+emotion_classifier = load_model(emotion_model,compile=False)
+ageNet=cv2.dnn.readNet(ageModel,ageProto)
+genderNet=cv2.dnn.readNet(genderModel,genderProto)
+
+
+def ageAndgender():
+
+    while True:
+        ret, img = frame.read()
+        default_img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+        face = face_cascade.detectMultiScale(image=default_img, scaleFactor=1.3, minNeighbors=5)
+        for x, y, w, h in face:
+            roi = default_img[y:y + h, x:x + w]
+            blob = cv2.dnn.blobFromImage(roi, 1.0, (227, 227), MODEL_MEAN_VALUES, swapRB=False)
+            genderNet.setInput(blob)
+            genderPreds = genderNet.forward()
+            gender = genderList[genderPreds[0].argmax()]
+            ageNet.setInput(blob)
+            agePreds = ageNet.forward()
+            age = ageList[agePreds[0].argmax()]
+            cv2.rectangle(img, (x, y), (x + w, y + h), (255, 0, 0), 2)
+            cv2.putText(img, f"{gender}, {age} year", (x, y - 5), cv2.FONT_HERSHEY_SIMPLEX, 0.8, (0, 0, 255))
+
+        cv2.imshow("Gender and Age Prediction", img)
+        key = cv2.waitKey(1) & 0xFF
+        if key == ord("q") or key == 27:
+                break
+    cv2.destroyAllWindows()
+
+def emotion():
+
+    while True:
+        ret, img = frame.read()
+        img2 = cv2.imread("./data/emojis/sad.png")
+        gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+        face = face_cascade.detectMultiScale(image=gray, scaleFactor=1.3, minNeighbors=5)
+
+        for x, y, w, h in face:
+
+            roi = gray[y:y + h, x:x + w]
+            roi = cv2.resize(roi, (48, 48))
+            roi = roi.astype("float") / 255.0
+            roi = img_to_array(roi)
+            roi = np.expand_dims(roi, axis=0)
+            preds = emotion_classifier.predict(roi)[0]
+            emotion_probability = np.max(preds)
+            label = Emotions[preds.argmax()]
+            cv2.rectangle(img, (x, y), (x + w, y + h), (0, 255, 0), 2)
+            cv2.putText(img, f"{label}", (x, y - 5), cv2.FONT_HERSHEY_SIMPLEX, 0.8, (0, 255, 0))
+            #cv2.face.drawFacemarks(img,)
+
+        cv2.imshow("Gender and Age Prediction", img)
+        key = cv2.waitKey(1) & 0xFF
+        if key == ord("q") or key == 27:
+                break
+    cv2.destroyAllWindows()
+
+    # img1 = cv2.imread('Desert.jpg')
+    # img2 = cv2.imread('Penguins.jpg')
+    #
+    # img3 = img1.copy()
+    # # replace values at coordinates (100, 100) to (399, 399) of img3 with region of img2
+    # img3[100:400, 100:400, :] = img2[100:400, 100:400, :]
+    # cv2.imshow('Result1', img3)
