4038c589c3364093e6b46fa1029b5c6e778dd3cb
JoeVenner <ylafrimi@gmail.com>
2020-07-12 13:10:47 +0100

    Update Detector.py

--- Detector.py
+++ Detector.py
@@ -1,13 +1,16 @@
 import cv2
-
-
+import pafy
+from time import sleep
+from PIL import Image 
 def main_app(name):
-
+        url = 'https://www.youtube.com/watch?v=lQnLwUfwgyA'
+        vpafy = pafy.new(url)
+        play = vpafy.getbest(preftype="mp4")
         face_cascade = cv2.CascadeClassifier('./data/haarcascade_frontalface_default.xml')
         recognizer = cv2.face.LBPHFaceRecognizer_create()
-        recognizer.read("./data/classifiers/"+name+"_classifier.xml")
-        cap = cv2.VideoCapture(0)
-
+        recognizer.read(f"./data/classifiers/{name}_classifier.xml")
+        cap = cv2.VideoCapture(play.url)
+        pred = 0
         while True:
             ret, frame = cap.read()
             #default_img = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
@@ -21,15 +24,18 @@ def main_app(name):
 
                 id,confidence = recognizer.predict(roi_gray)
                 confidence = 100 - int(confidence)
-                if confidence > 40:
+                pred = 0
+                if confidence > 50:
                     #if u want to print confidence level
                             #confidence = 100 - int(confidence)
+                            pred += +1
                             text = name.upper()
                             font = cv2.FONT_HERSHEY_PLAIN
                             frame = cv2.rectangle(frame, (x, y), (x + w, y + h), (0, 255, 0), 2)
                             frame = cv2.putText(frame, text, (x, y-4), font, 1, (0, 255, 0), 1, cv2.LINE_AA)
 
-                else:
+                else:   
+                            pred += -1
                             text = "UnknownFace"
                             font = cv2.FONT_HERSHEY_PLAIN
                             frame = cv2.rectangle(frame, (x, y), (x + w, y + h), (0, 0, 255), 2)
@@ -39,8 +45,32 @@ def main_app(name):
 
 
             if cv2.waitKey(20) & 0xFF == ord('q'):
+                print(pred)
+                if pred > 0 : 
+                    dim =(124,124)
+                    img = cv2.imread(f".\\data\\{name}\\{pred}{name}.jpg", cv2.IMREAD_UNCHANGED)
+                    resized = cv2.resize(img, dim, interpolation = cv2.INTER_AREA)
+                    cv2.imwrite(f".\\data\\{name}\\50{name}.jpg", resized)
+                    Image1 = Image.open(f".\\2.png") 
+                      
+                    # make a copy the image so that the  
+                    # original image does not get affected 
+                    Image1copy = Image1.copy() 
+                    Image2 = Image.open(f".\\data\\{name}\\50{name}.jpg") 
+                    Image2copy = Image2.copy() 
+                      
+                    # paste image giving dimensions 
+                    Image1copy.paste(Image2copy, (195, 114)) 
+                      
+                    # save the image  
+                    Image1copy.save("end.png") 
+                    frame = cv2.imread("end.png", 1)
+
+                    cv2.imshow("Result",frame)
+                    cv2.waitKey(5000)
                 break
+
 
         cap.release()
         cv2.destroyAllWindows()
-
+        
