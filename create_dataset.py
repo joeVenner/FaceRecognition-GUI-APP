66226832fa35abf273d7f809ce4f2d7241e6974f
JoeVenner <ylafrimi@gmail.com>
2020-07-12 13:09:28 +0100

    Update create_dataset.py

--- create_dataset.py
+++ create_dataset.py
@@ -17,7 +17,7 @@ def start_capture(name):
             grayimg = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
             face = detector.detectMultiScale(image=grayimg, scaleFactor=1.1, minNeighbors=5)
             for x, y, w, h in face:
-                cv2.rectangle(img, (x, y), (x+w, y+h), (255, 0, 0), 2)
+                cv2.rectangle(img, (x, y), (x+w, y+h), (0, 0, 0), 2)
                 cv2.putText(img, "Face Detected", (x, y-5), cv2.FONT_HERSHEY_SIMPLEX, 0.8, (0, 0, 255))
                 cv2.putText(img, str(str(num_of_images)+" images captured"), (x, y+h+20), cv2.FONT_HERSHEY_SIMPLEX, 0.8, (0, 0, 255))
                 new_img = img[y:y+h, x:x+w]
