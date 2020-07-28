ded238d4b1bc17c9be224b928623ca2769b23842
JoeVenner <ylafrimi@gmail.com>
2020-07-28 17:39:45 +0100

    Update Detector.py

--- Detector.py
+++ Detector.py
@@ -1,15 +1,13 @@
 import cv2
-import pafy
 from time import sleep
 from PIL import Image 
+
 def main_app(name):
-        url = 'https://www.youtube.com/watch?v=lQnLwUfwgyA'
-        vpafy = pafy.new(url)
-        play = vpafy.getbest(preftype="mp4")
+        
         face_cascade = cv2.CascadeClassifier('./data/haarcascade_frontalface_default.xml')
         recognizer = cv2.face.LBPHFaceRecognizer_create()
         recognizer.read(f"./data/classifiers/{name}_classifier.xml")
-        cap = cv2.VideoCapture(play.url)
+        cap = cv2.VideoCapture(0)
         pred = 0
         while True:
             ret, frame = cap.read()
